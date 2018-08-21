c112266e6711f875ce42979900f3ebec24ecd9aa
Rodhes <rodhesc@hotmail.com>
2018-08-21 11:18:08 -0300

    Testando a primeira classe de dominio categoria

---
+++ src/main/java/com/rdtelecom/cursomv/domain/Categoria.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rdtelecom.cursomv.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + Objects.hashCode(this.id);
+        hash = 19 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nome=" + nome + '}';
+    }
+    
+
+    
+}
--- src/main/java/com/rdtelecom/cursomv/resources/CategoriaResource.java
+++ src/main/java/com/rdtelecom/cursomv/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.rdtelecom.cursomv.resources;
 
+import com.rdtelecom.cursomv.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +14,16 @@ public class CategoriaResource {
     
     
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando";
+    public List<Categoria> listar(){
+          
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Esctitório");
+        
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+      
+        return lista;
     }
 
 }
